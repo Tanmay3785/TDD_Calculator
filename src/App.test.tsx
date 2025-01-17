47586d105b46253196cedc9b86d2a59aa74112f7
Tanmay3785 <tanmay.waghole@humancloud.co.in>
2025-01-17 16:19:44 +0530

    Add failing test for initial rendering of Calculator component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import Calculator from "./components/Calculator";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders calculator component correctly", () => {
+  render(<Calculator />);
+  const result = screen.getByTestId("result");
+  expect(result).toBeInTheDocument();
+  const button = screen.getByText("1");
+  expect(button).toBeInTheDocument();
 });
